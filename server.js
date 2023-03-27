80d3129c4795ee5a75c50f22bdeb08220aeb4a14
Nicola <nicola.corvo90@gmail.com>
2023-03-27 20:00:26 +0200

    Edited host

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ fastify.register(require('./routes/order'));
   
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000, host: '0.0.0.0' })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
